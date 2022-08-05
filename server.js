810b0acb2bbb65a70ee96027c7297d76af7c7c23
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-08-05 14:01:30 -0400

    trying agin

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:3001/b-track";
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://locallost:3001/b-track', { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:3001/b-track", { 
   useNewUrlParser: true,
   useFindAndModify: false
 });
