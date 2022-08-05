89b371b750041aa4d012f6e7f3d80dd3bfc5e3c3
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-08-05 13:25:14 -0400

    adding service

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -19,3 +19,54 @@ const FILES_TO_CACHE = [
     '/icons/icon-72x72.png',
 ];
 
+self.addEventListener('install', function (e) {
+    e.wait(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('cache install :' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
+
+self.addEventListener('activate', function (e) {
+    e.wait(
+        cache.keys().then(function (keyList) {
+            let cacheKeep = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+
+            cacheKeep.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeep.indexOf(key) === -1) {
+                    console.log('delete cache :' + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            })
+        )}
+    )  
+    )    
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request :' + e.request.url)
+
+    e.respond(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding :' + e.request.url)
+                return request
+                } else {
+                    console.log('Not cached, fetching : ' + e.request.url)
+                    return fetch(e.request)
+                }
+            }
+        )
+    )
+});
+
+
+
+
+
+
