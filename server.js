eab86d641d646902dc2531cf8fe4c5755ce75fb2
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-08-06 10:17:45 -0400

    heroku

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:3001/b-track", { 
+mongoose.connect(MONGODB_URI, { 
   useNewUrlParser: true,
   useFindAndModify: true
 });
